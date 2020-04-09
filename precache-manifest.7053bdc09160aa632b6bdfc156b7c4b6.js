a6f160014c1a0278585cd6224095195030c73ba8
Nazar-Shtypka <nazar.schtypka@gmail.com>
2020-04-09 14:14:53 +0300

    Updates

---
+++ precache-manifest.7053bdc09160aa632b6bdfc156b7c4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74edb096f298be8483dc819d8ad7d950",
+    "url": "/Timer_React/index.html"
+  },
+  {
+    "revision": "de2a6a9fefae8924b61c",
+    "url": "/Timer_React/static/css/main.ee07ce7f.chunk.css"
+  },
+  {
+    "revision": "b1053270a634d186ce82",
+    "url": "/Timer_React/static/js/2.7ad55116.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Timer_React/static/js/2.7ad55116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de2a6a9fefae8924b61c",
+    "url": "/Timer_React/static/js/main.d8c97515.chunk.js"
+  },
+  {
+    "revision": "c9d137d35dc5cbde04d3",
+    "url": "/Timer_React/static/js/runtime-main.883dc202.js"
+  }
+]);
